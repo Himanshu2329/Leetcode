a7123a270567568a244e62d401fc4c71de7ecdc6
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-02-06 23:46:15 +0530

    1365 Done

---
+++ Leet1365.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Leet1365
+ */
+public class Leet1365 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=sc.nextInt();
+        }
+        int res[]=getSmallNumber(arr,n);
+        System.out.println(res);
+    }
+
+    private static int[] getSmallNumber(int[] arr, int n) {
+        // TODO Auto-generated method stub
+        int []res=new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            int cnt=0;
+            for (int j = 0; j < arr.length; j++) {
+                if(arr[i]>arr[j]){
+                    cnt++;
+                }
+                res[i]=cnt;
+            }
+        }
+
+        return res;
+        // throw new UnsupportedOperationException("Unimplemented method 'getSmallNumber'");
+    }
+}
