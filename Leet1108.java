d91c9231b2079de924025713c170ca8f9119dbe6
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-02-07 23:11:33 +0530

    Leet 1108 Done

---
+++ Leet1108.java
@@ -0,0 +1,11 @@
+/**
+ * Leet1108
+ */
+public class Leet1108 {
+
+    public static void main(String[] args) {
+        String s1="1.1.1.1";
+        System.out.println(s1.replace(".", "[.]"));
+        
+    }
+}
