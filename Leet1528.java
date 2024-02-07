dd2849a0c57b2ea0f3456d08378e51160b6c786e
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-02-08 00:06:25 +0530

    1528 Done

--- Leet1528.java
+++ Leet1528.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -10,12 +10,13 @@ public class Leet1528 {
         String s = "codeleet";
         int index[] = { 4, 5, 6, 7, 0, 2, 1, 3 };
         // ArrayList <String> List=new ArrayList<>();
-        int res[] = new int[index.length];
+       char res[] = new char[index.length];
         for (int i = 0; i < index.length; i++) {
             res[index[i]]=s.charAt(i);
 
         }
-        String str=Arrays.toString(res);
+        String str= new String(res);
         System.out.println(str);
+
     }
 }
