3e4be9f2278581682bd4fa289f7f01c523d5b5c8
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-02-09 23:40:27 +0530

    leet 1832 Done

--- Leet1832.java
+++ Leet1832.java
@@ -4,14 +4,27 @@ import java.util.Set;
 public class Leet1832 {
     public static void main(String[] args) {
         String s="thequickbrownfoxjumpoverthelazydog" ;
-        Set <Character> set=new HashSet<>();
-        for (int i = 0; i < s.length(); i++) {
-            set.add(s.charAt(i));            
-        }
-        if (set.size()==26) 
-            System.out.println(true);
+        // Set <Character> set=new HashSet<>();
+        // for (int i = 0; i < s.length(); i++) {
+        //     set.add(s.charAt(i));            
+        // }
+        // if (set.size()==26) 
+        //     System.out.println(true);
 
-        else
-            System.out.println(false);
+        // else
+        //     System.out.println(false);
+
+        boolean arr[]=new boolean[26];
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i)-'a'] = true;            
+        }
+        for (boolean b : arr) {
+            if (!b) {
+                System.out.println(false);
+                break;
+            }   else
+                    System.out.println(true);
+                    break;
+        }
     }
 }
