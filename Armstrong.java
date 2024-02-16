a8402fb2962092eef02c40db68946601e5e7b3b9
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-02-16 23:35:47 +0530

    Armstrong

---
+++ Armstrong.java
@@ -0,0 +1,28 @@
+public class Armstrong {
+    public static void main(String[] args) {
+        int n = 1634;
+        boolean res=getArm(n);
+        System.out.println(res);
+
+    }
+
+    private static boolean getArm(int n) {
+        // TODO Auto-generated method stub
+        int digit=0;
+        int result=0;
+        int temp=n;
+        while (temp!=0) {
+            temp/=10;
+            digit++;
+        }
+        int temp1=n;
+        while (temp1!=0) {
+            int res=temp1%10;
+           result+=Math.pow(res, digit);
+            temp1/=10;
+        }
+        return result==n;
+        // return result;
+        // throw new UnsupportedOperationException("Unimplemented method 'getArm'");
+    }
+}
