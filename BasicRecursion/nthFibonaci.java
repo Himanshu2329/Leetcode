896db25ca8c0200d381e0fba37b970c45c91152a
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-03-02 21:54:59 +0530

    Fibo Done

---
+++ BasicRecursion/nthFibonaci.java
@@ -0,0 +1,19 @@
+package BasicRecursion;
+
+public class nthFibonaci {
+    public static void main(String[] args) {
+        int num=7;
+        int res =getFib(num);
+        System.out.println(res);
+    }
+
+    private static int getFib(int num) {
+        // TODO Auto-generated method stub
+        if(num <2){
+            return num;
+        }
+          return  getFib(num-2)+getFib(num-1);
+        // throw new UnsupportedOperationException("Unimplemented method 'getFib'");
+    }
+    
+}
