e5216a80bbe4bc673bea39573d18e048e5b7fa1a
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-03-04 23:08:51 +0530

    CountDigit.java

--- CountDigit.java
+++ CountDigit.java
@@ -4,7 +4,7 @@
 public class CountDigit {
 
     public static void main(String[] args) {
-        int n = 12345;
+        int n = 12345; 
         int cnt=0; 
         while (n>0) {
             int temp=n%10;
