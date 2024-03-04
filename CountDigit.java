9e0c912f8529edca4e85ca026b241487e26cab1d
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-03-04 23:09:06 +0530

    CountDigit.java

--- CountDigit.java
+++ CountDigit.java
@@ -8,7 +8,7 @@ public class CountDigit {
         int cnt=0; 
         while (n>0) {
             int temp=n%10;
-            cnt++;
+            cnt++; 
             n/=10;
         }
         System.out.println(cnt);
