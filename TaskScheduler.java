7007f7e5dc4b5b7451bdb31fbed2e70ee35e01c7
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-03-19 23:19:05 +0530

    Create TaskScheduler.java

---
+++ TaskScheduler.java
@@ -0,0 +1,23 @@
+class Solution {
+         public int leastInterval(char[] tasks, int n) {
+            if (n == 0) {
+                return tasks.length;
+            }
+            int[] f = new int[26];
+            for (char c : tasks) {
+                f[c - 'A']++;
+            }
+            int max = 0;
+            int count = 0;
+            for (int i : f) {
+                if (i > max) {
+                    max = i;
+                    count = 1;
+                } else if (i == max) {
+                    count++;
+                }
+
+            }
+            return Math.max(tasks.length, (max - 1) * (n + 1) + count);
+            }
+    }
