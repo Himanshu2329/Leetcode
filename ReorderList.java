d43780c7caa4ab503492a1248df98043292fd394
Himanshu2329 <96252633+Himanshu2329@users.noreply.github.com>
2024-03-23 23:06:26 +0530

    Create ReorderList.java

---
+++ ReorderList.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+     public static void reorderList(ListNode head) {
+		if (head.next == null) {
+			return;
+		}
+		reorderList2(head, head.next);
+	}
+
+	public static ListNode reorderList2(ListNode head, ListNode curr) {
+		ListNode temp;
+		if (curr.next != null) {
+			temp = reorderList2(head, curr.next);
+		} else {
+			temp = head;
+		}
+		if(temp == null) {
+			return null;
+		}
+		if (temp == curr  || temp.next == curr) {
+			curr.next=null;
+			return null;
+		}
+
+		curr.next = temp.next;
+		temp.next = curr;
+		return curr.next;
+	}
+        
+}
